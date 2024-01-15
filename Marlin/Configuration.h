82e4a43b3956ec909e1a638a166859bd00b6e60d
Jimmy White <jimmyeao@users.noreply.github.com>
2024-01-15 17:39:30 +0000

    led startup check

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -3300,7 +3300,7 @@
   #define NEOPIXEL_PIXELS              26 // Number of LEDs in the strip. (Longest strip when NEOPIXEL2_SEPARATE is disabled.)
   #define NEOPIXEL_IS_SEQUENTIAL          // Sequential display for temperature change - LED by LED. Disable to change all LEDs at once.
   #define NEOPIXEL_BRIGHTNESS         127 // Initial brightness (0-255)
-  //#define NEOPIXEL_STARTUP_TEST         // Cycle through colors at startup
+  #define NEOPIXEL_STARTUP_TEST         // Cycle through colors at startup
 
   // Support for second Adafruit NeoPixel LED driver controlled with M150 S1 ...
   //#define NEOPIXEL2_SEPARATE
